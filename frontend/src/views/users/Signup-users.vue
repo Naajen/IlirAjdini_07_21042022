b092f44a0090f13b1d53733ad7e177b6a2a99cf6
Ilir <ajdini.ilir.91@gmail.com>
2022-04-29 22:54:13 +0200

    ajustement du formulaire d'inscription

--- frontend/src/views/users/Signup-users.vue
+++ frontend/src/views/users/Signup-users.vue
@@ -4,11 +4,23 @@
     <h4>Pas encore de compte ? Inscrivez-vous</h4>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-        <input type="nom" placeholder="Pseudo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" placeholder="Pseudo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Nom</label>
+        <input type="text" placeholder="Nom" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Prénom</label>
+        <input type="text" placeholder="Prénom" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">E-Mail</label>
+        <input type="email" placeholder="E-Mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="myInput" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Il faut un minimum de 8 caractères" required>
+        <input type="password" class="form-control" id="myInput" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Il faut un minimum de 8 caractères, 1 Majuscule et 1 Chiffre" required>
       </div>
         <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input"  @click="seeYou()" id="exampleInputPassword1">
@@ -48,9 +60,9 @@ form {
 }
 .main {
     display: flex;
+    display: flex;
+    display: flex;
     justify-content: center;
     margin-top:50px;
 }
-
-
 </style>
