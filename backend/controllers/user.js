6e5c24677cfbbc965a279870188c9e9920e11d39
Ilir <ajdini.ilir.91@gmail.com>
2022-05-03 23:11:40 +0200

    exports login en partie terminer token généré & vérification de compte existant dans la BDD

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,13 @@
 const bcrypt = require('bcrypt');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const connectionDB = require("../utils/mysqlDB")
 
 // S'inscrire
 exports.signup = (req, res) => {
-  bcrypt.hash(req.body.password,10)
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+  const sqlInsert = 'INSERT INTO users SET ?';
+  if (regex.test(req.body.password)) {
+    bcrypt.hash(req.body.password,10)
     .then((hash) => {
       const user = {
         name: req.body.name,
@@ -12,8 +15,7 @@ exports.signup = (req, res) => {
         password:hash
       }
       //request SQL table users ? évite les injection SQL on envoi le array user après le INSERT
-      connectionDB.query(
-        'INSERT INTO users SET ?', user, (error, results, fields) => {
+      connectionDB.query(sqlInsert, user, (error, results, fields) => {
           if (error) {
             res.json ({error});
           } else {
@@ -23,23 +25,40 @@ exports.signup = (req, res) => {
       )
     })
     .catch((error) => res.status(500).json({error}))
+  } else {
+    return res.status(401).json({
+      message: 'Le mot de passe doit avoir au moins 8 caractères, un chiffre, et une majuscule'
+    })
+  }
+
 }
-exports.login = (req, res) => {
+exports.login = (req, res, next) => {
     const email = req.body.email;
-    connectionDB.query(
-      'SELECT * FROM users WHERE email = ?', email, (error, results, fields) => {
-        if (error) {
-          res.json ({error});
-        } else {
-          res.json ({ message : results[0].email});
+    const sqlMail = 'SELECT * FROM users WHERE email = ?';
+    //vérifier si le compte est déjà existant
+    connectionDB.query(sqlMail, email, (err, results, fields) => {
+        if (results == 0) {
+          return res.status(403).json({ error: 'Utilisateur inéxistant'});
         }
-      }
-    )
-
+        bcrypt.compare(req.body.password, results[0].password)
+        .then((passwordValid) => {
+          if (!passwordValid) {
+          //si le mdp est INCORRECT
+          return res.status(403).json({ error: 'Mdp incorrect'});
+          }
+          //si le mdp est  CORRECT
+          const token = jwt.sign( 
+            {userId: results[0].id}, 
+            'RANDOM_TOKEN_SECRET', 
+            {expiresIn: '24h'}
+          )
+          //reponse du serve avec userId et le token
+          return res.status(200).json ({
+            userId: results[0].id,
+            token
+          })
+        })
+        .catch((error) => res.status(500).json({ error }))
+    })
 }
 
-
-
-
-
-//Emplacement du CRUD pour la DB >>> à voir comment cela fonctionne getDB.db.query("SELECT * FROM ma_table");
