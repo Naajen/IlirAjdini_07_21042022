3291a6844ecc9213bf7753c830238cdd0b4a236c
Ilir <ajdini.ilir.91@gmail.com>
2022-05-03 18:49:46 +0200

    controller login à été réaliser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,19 +11,31 @@ exports.signup = (req, res) => {
         email: req.body.email,
         password:hash
       }
-      //request SQL table users
+      //request SQL table users ? évite les injection SQL on envoi le array user après le INSERT
       connectionDB.query(
         'INSERT INTO users SET ?', user, (error, results, fields) => {
           if (error) {
             res.json ({error});
           } else {
-            console.log(results);
             res.json ({ message : "users créer"});
           }
         }
       )
     })
     .catch((error) => res.status(500).json({error}))
+}
+exports.login = (req, res) => {
+    const email = req.body.email;
+    connectionDB.query(
+      'SELECT * FROM users WHERE email = ?', email, (error, results, fields) => {
+        if (error) {
+          res.json ({error});
+        } else {
+          res.json ({ message : results[0].email});
+        }
+      }
+    )
+
 }
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,7 +7,7 @@ const userCtrl = require('../controllers/user');
 //la route (endpoint) signup + le mdlware password doit avoir un mdp plus élaboré
 router.post('/signup', userCtrl.signup);
 //la route (endpoint) login
-//router.post('/login',  userCtrl.login);
+router.post('/login',  userCtrl.login);
 
 //exportation du module
 module.exports = router;
